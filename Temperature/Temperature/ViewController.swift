d928a1f744d11e4af1e7e4b8ed989dcca65ab5a3
VM_macOS <vm_macos@VM-macOSs-Mac.local>
2020-11-02 17:25:18 +0300

    Temparature folder added

---
+++ Temperature/Temperature/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Temperature
+//
+//  Created by VM_macOS on 02.11.2020.
+//  Copyright © 2020 NickolayKryuchkov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
