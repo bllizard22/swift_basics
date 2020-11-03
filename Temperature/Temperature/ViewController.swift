2bdfe5b5c9af3b6bedc8d9c2dcebb4ece68b9fcc
VM_macOS <vm_macos@VM-macOSs-Mac.local>
2020-11-03 14:23:30 +0300

    Redesigned GUI

--- Temperature/Temperature/ViewController.swift
+++ Temperature/Temperature/ViewController.swift
@@ -10,10 +10,31 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var swapButton: UIButton!
+    
+    @IBOutlet weak var unitsSegments1: UISegmentedControl!
+    @IBOutlet weak var unitsSegments2: UISegmentedControl!
+
+    @IBOutlet weak var inputTextField: UITextField!
+    @IBOutlet weak var outputLabel: UILabel!
+    
+    @IBAction func valueDidChanged(_ sender: UITextField) {
+        outputLabel.text = sender.text ?? "1.00"
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        swapButton.layer.cornerRadius = 10
+        swapButton.layer.borderWidth = 2
+        swapButton.layer.borderColor = UIColor(red: 0.75, green: 0.75, blue: 0.75, alpha: 1.0).cgColor
     }
+    
+//    overr override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+//
+//    }
+    
+    
 
 
 }
