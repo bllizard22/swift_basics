e632e25c949173e43cb78939fc0a87314253e8f8
VM_macOS <vm_macos@VM-WS-macOS.local>
2020-11-09 17:31:40 +0300

    Added TabBar Scratch

---
+++ tabBar/tabBar/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  tabBar
+//
+//  Created by VM_macOS on 09.11.2020.
+//  Copyright © 2020 NickolayKryuchkov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
