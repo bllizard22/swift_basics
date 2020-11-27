5c3a112b7ca7fb517f8b509f4e5f5bd116e0ccbd
Nick <bllizard9536@gmail.com>
2020-11-27 16:25:34 +0300

    Xib testing project added

---
+++ xibTestingProject/xibTestingProject/MyView.swift
@@ -0,0 +1,52 @@
+//
+//  MyView.swift
+//  xibTestingProject
+//
+//  Created by nick on 11/27/20.
+//
+
+import UIKit
+
+@IBDesignable class MyView: UIView {
+
+    @IBOutlet weak var textLabelTest: UILabel!
+    
+    @IBInspectable var textLabelHere: String {
+        get {
+            return textLabelTest.text!
+        }
+        
+        set(textLabelHere) {
+            textLabelTest.text = textLabelHere
+        }
+    }
+    
+    var view: UIView!
+    var nibName: String = "MyView"
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        setup()
+    }
+    
+    func loadFromNib() -> UIView {
+        let bundle = Bundle(for: type(of: self))
+        let nib = UINib(nibName: nibName, bundle: bundle)
+        let view = nib.instantiate(withOwner: self, options: nil).first as! UIView
+        
+        return view
+    }
+    
+    func setup() {
+        view = loadFromNib()
+        view.frame = bounds
+        view.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        
+        addSubview(view)
+    }
+
+}
---
+++ xibTestingProject/xibTestingProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  xibTestingProject
+//
+//  Created by nick on 11/27/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
